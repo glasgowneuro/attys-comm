b88bb7d3620ab8bd4163d0bb53de85193c8bd846
Bernd Porr <mail@berndporr.me.uk>
2020-03-07 01:22:03 +0000

    Cleanup now removes the .so properly

--- cleanup.sh
+++ cleanup.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-rm -rf libattyscomm.so.* cmake_install.cmake
+rm -rf cmake_install.cmake libattyscomm.so*
 rm -rf dist pyattyscomm_wrap.cpp pyattyscomm_wrap.h __pycache__ pyattyscomm.egg-info build pyattyscomm.py
 rm -rf CMakeCache.txt CMakeFiles 
