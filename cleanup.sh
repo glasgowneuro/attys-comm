a85c1cbbcf49c1ea06d4c81c20bdf08a285cbd1b
Bernd Porr <mail@berndporr.me.uk>
2020-03-07 01:17:18 +0000

    Should also remove pyattyscomm.py

--- cleanup.sh
+++ cleanup.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 rm -rf libattyscomm.so.* cmake_install.cmake
-rm -rf dist pyattyscomm_wrap.cpp pyattyscomm_wrap.h __pycache__ pyattyscomm.egg-info build
+rm -rf dist pyattyscomm_wrap.cpp pyattyscomm_wrap.h __pycache__ pyattyscomm.egg-info build pyattyscomm.py
 rm -rf CMakeCache.txt CMakeFiles 
