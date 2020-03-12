fe23cb9fa923b9ec255b79af3c6c7ba55f6e0dbf
Bernd Porr <mail@berndporr.me.uk>
2020-03-12 23:34:02 +0000

    Added scantest

---
+++ test/scantest.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "AttysComm.h"
+#include "AttysScan.h"
+
+
+struct MyMessage : public AttysCommMessage {
+	virtual void hasMessage(int, const char* s) {
+		printf("Message = %s\n",s);
+	}
+};
+
+
+
+
+int main (int,char**) {
+	MyMessage mymessage;
+	// scan for Attys
+	attysScan.scan(4);
+	if (attysScan.getNAttysDevices() < 1) {
+		printf("No devices found.\n");
+		return 0;
+	}
+	for(int i=0;i<attysScan.getNAttysDevices();i++) {
+		printf("%s\n",attysScan.attysName[i]);
+		attysScan.getAttysComm(i)->start();
+	}
+	std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+	for(int i=0;i<attysScan.getNAttysDevices();i++) {
+		attysScan.getAttysComm(i)->quit();
+	}
+	return 0;
+}
