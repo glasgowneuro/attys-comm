84e7e5d3e49fd3fbea1f9da359e3faaa9f627d01
Bernd Porr <mail@berndporr.me.uk>
2020-07-24 23:31:09 +0100

    Sampling rate back to 250Hz

--- examples/attysdemo.cpp
+++ examples/attysdemo.cpp
@@ -42,7 +42,7 @@ int main (int,char**) {
 	attysScan.getAttysComm(0)->registerCallback(&mysample);
 	attysScan.getAttysComm(0)->registerMessageCallback(&mymessage);
 	// start the data acquisition in the background
-	attysScan.getAttysComm(0)->setAdc_samplingrate_index(AttysCommBase::ADC_RATE_500HZ);
+	attysScan.getAttysComm(0)->setAdc_samplingrate_index(AttysCommBase::ADC_RATE_250HZ);
 	attysScan.getAttysComm(0)->start();
 	getchar();
 	attysScan.getAttysComm(0)->quit();
