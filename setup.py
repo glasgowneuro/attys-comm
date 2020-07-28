1237d40601d06bba041919566b7be7820c26a26f
Bernd Porr <mail@berndporr.me.uk>
2020-07-28 13:51:26 +0100

    Bumped up a python version because of a leftover old symbol

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ elif platform == "darwin":
 
 						   
 setup (name = 'pyattyscomm',
-       version = '2.0.14.2',
+       version = '2.0.14.3',
        author      = "Bernd Porr",
        author_email = "bernd@glasgowneuro.tech",
        url = "https://github.com/glasgowneuro/attys-comm",
